73bae0bf05de87f2634538e6fbae166bd30a3ded
Mahmoud Alshukri <mahmoud.h.alshukri@gmail.com>
2025-05-04 15:27:57 +0300

    trying minimal version of admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,47 +1,57 @@
-import Announcements from "@/components/Announcements";
-import AttendenceChartContainer from "@/components/AttendenceChartContainer";
-import CountChartContainer from "@/components/CountChartContainer";
-import EventCalendarContainer from "@/components/EventCalendarContainer";
-import FinanceChart from "@/components/FinanceChart";
-import UserCard from "@/components/UserCard";
+// import Announcements from "@/components/Announcements";
+// import AttendenceChartContainer from "@/components/AttendenceChartContainer";
+// import CountChartContainer from "@/components/CountChartContainer";
+// import EventCalendarContainer from "@/components/EventCalendarContainer";
+// import FinanceChart from "@/components/FinanceChart";
+// import UserCard from "@/components/UserCard";
 
-const AdminPage = async ({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) => {
+// const AdminPage = async ({
+//   searchParams,
+// }: {
+//   searchParams: { [key: string]: string | undefined };
+// }) => {
+//   return (
+//     <div className="p-4 flex gap-4 flex-col md:flex-row">
+//       {/* LEFT */}
+//       <div className="w-full lg:w-2/3 flex flex-col gap-8">
+//         {/* USER CARDS */}
+//         <div className="flex gap-4 justify-between flex-wrap">
+//           <UserCard type="admin" />
+//           <UserCard type="student" />
+//           <UserCard type="teacher" />
+//           <UserCard type="parent" />
+//         </div>
+//         {/* MIDDLE CHART */}
+//         <div className="flex gap-4 flex-col lg:flex-row">
+//           {/* COUNT CHART */}
+//           <div className="w-full lg:w-1/3 h-[450px]">
+//             <CountChartContainer />
+//           </div>
+//           {/* ATTENDANCE CHART */}
+//           <div className="w-full lg:w-2/3 h-[450px]">
+//             <AttendenceChartContainer />
+//           </div>
+//         </div>
+//         {/* BOTTOM CHART */}
+//         <div className="w-full h-[500px]">
+//           <FinanceChart />
+//         </div>
+//       </div>
+//       {/* RIGHT */}
+//       <div className="w-full lg:w-1/3 flex flex-col gap-8">
+//         <EventCalendarContainer searchParams={searchParams} />
+//         <Announcements />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AdminPage;
+// app/admin/page.tsx (temporary minimal version)
+const AdminPage = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* LEFT */}
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* USER CARDS */}
-        <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="admin" />
-          <UserCard type="student" />
-          <UserCard type="teacher" />
-          <UserCard type="parent" />
-        </div>
-        {/* MIDDLE CHART */}
-        <div className="flex gap-4 flex-col lg:flex-row">
-          {/* COUNT CHART */}
-          <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChartContainer />
-          </div>
-          {/* ATTENDANCE CHART */}
-          <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendenceChartContainer />
-          </div>
-        </div>
-        {/* BOTTOM CHART */}
-        <div className="w-full h-[500px]">
-          <FinanceChart />
-        </div>
-      </div>
-      {/* RIGHT */}
-      <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendarContainer searchParams={searchParams} />
-        <Announcements />
-      </div>
+    <div>
+      <h1>Admin Page Working</h1>
     </div>
   );
 };
