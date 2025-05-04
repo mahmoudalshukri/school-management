c6c003b295f7ffc2470faa54d6f5ccf00516132c
Mahmoud Alshukri <mahmoud.h.alshukri@gmail.com>
2025-05-04 16:18:48 +0300

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  datasources: {
+    db: {
+      url:
+        process.env.DATABASE_URL +
+        (process.env.NODE_ENV === "production"
+          ? "?connection_limit=5&pool_timeout=15"
+          : ""),
+    },
   },
 };
 
