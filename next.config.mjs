59fdb37254e71235395227bda37778c977a948bf
Mahmoud Alshukri <mahmoud.h.alshukri@gmail.com>
2025-05-04 16:26:09 +0300

    add docker file

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
-  datasources: {
-    db: {
-      url:
-        process.env.DATABASE_URL +
-        (process.env.NODE_ENV === "production"
-          ? "?connection_limit=5&pool_timeout=15"
-          : ""),
-    },
-  },
 };
 
 export default nextConfig;
